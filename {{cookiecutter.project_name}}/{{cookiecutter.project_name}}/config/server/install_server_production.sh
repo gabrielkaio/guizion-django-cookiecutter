ac8e254ae9b1652322f6848637712e1393d2573f
Lucas Mrowskovsky Paim <lucasmpaim1@gmail.com>
2017-06-18 12:34:01 -0300

    Update install_server_production.sh

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/config/server/install_server_production.sh
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/config/server/install_server_production.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 export VIRTUALENV_NAME="{{cookiecutter.virtual_env_name}}";
-export GIT_LINK "git@bitbucket.com:guizion/{{git_project_name}}.git";
-export GIT_PROJECT_NAME "{{git_project_name}}";
+export GIT_LINK "git@bitbucket.com:guizion/{{cookiecutter.git_project_name}}.git";
+export GIT_PROJECT_NAME "{{cookiecutter.git_project_name}}";
 
 export RABBITMQ_USER "{{cookiecutter.rabbitmq_user}}";
 export RABBITMQ_PASSWORD "{{cookiecutter.rabbitmq_password}}";
