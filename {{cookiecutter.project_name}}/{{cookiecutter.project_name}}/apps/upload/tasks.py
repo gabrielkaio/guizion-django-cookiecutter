6e162b7a61c3e7764023c41446e0374aa92b1ab3
Lucas Mrowskovsky Paim <lucasmpaim1@gmail.com>
2017-06-18 13:00:55 -0300

    Create tasks.py

---
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/apps/upload/tasks.py
@@ -0,0 +1,12 @@
+import base64
+
+from celery.task import task
+
+from judzi_ws.apps.upload.upload_photo import PhotoUpload
+
+
+@task()
+def upload_in_background(file_base64, key):
+    file = base64.b64decode(file_base64.encode('utf-8'))
+    file_upload = PhotoUpload()
+    file_upload.do_request_upload(file, key)
